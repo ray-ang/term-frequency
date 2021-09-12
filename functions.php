44fb55d1db92a78f4ebf29cfcc6d790035b9ec79
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-09-12 05:43:15 -0400

    Count repeating terms as well in query document

--- functions.php
+++ functions.php
@@ -30,7 +30,6 @@ function preproc($text) {
 		}
 	}
 
-	$tokens = array_unique($tokens); // remove duplicates
 	return $tokens;
 }
 
@@ -50,27 +49,26 @@ function tf_total($input, $topic_terms, $threshold) {
 	if (! is_integer($threshold) || $threshold < 0) return '$threshold parameter should be a positive integer.';
 
 	$input_tokens = preproc($input); // preprocess input
-	ksort($input_tokens); // sort keys ascending
 
 	$term_tokens = array();
 	foreach ($topic_terms as $key => $value) { // preprocess topic terms
 		$term_tokens[$key] = preproc($value);
 	}
-	ksort($term_tokens); // sort keys ascending
 
 	$num_tokens = count($input_tokens);
 	$threshold = $threshold / $num_tokens; // normalized threshold
 
 	$tf_array = array();
 	foreach ($term_tokens as $key => $value) {
-		$tf_raw = 0;
 		$tf_t = 0;
 		foreach ($value as $term) {
+			$tf_raw = 0;
 			if (in_array($term, $input_tokens)) { // raw TF
-				$tf_raw += 1;
+				$tf_raw = array_count_values($input_tokens)[$term];
 			}
+
+			$tf_t += $tf_raw / $num_tokens; // Total normalized TF
 		}
-		$tf_t += $tf_raw / $num_tokens; // Total normalized TF
 
 		if ($tf_t >= $threshold) { // signal detection
 			$tf_array[$key] = $tf_t; // recommended topics
